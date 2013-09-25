86a11c1523d83f0fc8fb9c49efd83b78553c1311
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2013-09-25 21:56:23 +0200

    More tests

--- tests/tests.js
+++ tests/tests.js
@@ -4,8 +4,8 @@ test('Attributes', function () {
 	ok($('#link').removeAttr('title'), 'Remove attribute');
 	equal($('#link').attr('title'), null, 'Get null title attribute');
 
-	ok($('#span').attr('lang', 'en'), 'Set lang attribute');
-	equal($('#span').attr('lang'), 'en', 'Get lang attribute');
+	ok($('#span1').attr('lang', 'en'), 'Set lang attribute');
+	equal($('#span1').attr('lang'), 'en', 'Get lang attribute');
 
 	equal($('#text').is('input'), true, 'is (input)');
 
@@ -13,25 +13,51 @@ test('Attributes', function () {
 
 	ok($('#text').val('Bye World!'), 'Set val test');
 	equal($('#text').val(), 'Bye World!', 'Get val test 2');
+
+	equal($('#link').html(), 'Google link', 'Get html test 1');
+	equal($('#link').text(), 'Google link', 'Get text test 1');
+
+	ok($('#link').html('<span>New link</span>'), 'Set html test');
+	equal($('#link').html(), '<span>New link</span>', 'Get html test 2');
+
+	ok($('#link').text('Another link'), 'Set text test');
+	equal($('#link').text(), 'Another link', 'Get text test 2');
 });
 
 test('Class', function () {
-	ok($('#span').addClass('blue'), 'addClass');
-	equal($('#span').hasClass('blue'), true, 'hasClass (true)');
+	ok($('#span1').addClass('blue'), 'addClass');
+	equal($('#span1').hasClass('blue'), true, 'hasClass (true)');
 
-	ok($('#span').removeClass('blue'), 'removeClass');
-	equal($('#span').hasClass('blue'), false, 'hasClass (false)');
+	ok($('#span1').removeClass('blue'), 'removeClass');
+	equal($('#span1').hasClass('blue'), false, 'hasClass (false)');
 
-	ok($('#span').toggleClass('blue'), 'toggleClass');
-	equal($('#span').hasClass('blue'), true, 'hasClass (toogle true)');
+	ok($('#span1').toggleClass('blue'), 'toggleClass');
+	equal($('#span1').hasClass('blue'), true, 'hasClass (toogle true)');
 
-	ok($('#span').toggleClass('blue'), 'toggleClass');
-	equal($('#span').hasClass('blue'), false, 'hasClass (toogle false)');
+	ok($('#span1').toggleClass('blue'), 'toggleClass');
+	equal($('#span1').hasClass('blue'), false, 'hasClass (toogle false)');
 });
 
 test('CSS', function () {
 	equal($('#div').css('display'), 'block', 'Get display property');
 
-	ok($('#span').css('position', 'relative'), 'Set position property');
-	equal($('#span').css('position'), 'relative', 'Get position property');
+	ok($('#span1').css('position', 'relative'), 'Set position property');
+	equal($('#span1').css('position'), 'relative', 'Get position property');
+});
+
+test('Traversing', function () {
+	equal($('span').filter('#span1').length, 1, 'Filter test');
+
+	equal($('body').find('.child').length, 3, 'Find test');
+
+	equal($('#div').parent().is('body'), true, 'Parent test');
+
+	equal($('#span2').prev().is('#span1'), true, 'Prev test');
+
+	equal($('#span2').next().is('#span3'), true, 'Next test');
+
+	equal($('#parent').children().length, 3, 'Children test');
+
+	ok($('#remove').remove(), 'Remove test');
+	equal($('#remove').length, 0, 'Remove test');
 });
